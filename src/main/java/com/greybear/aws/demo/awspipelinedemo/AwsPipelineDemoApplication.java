b7a60d4b2f594feebf03e2f2a3a0e4c199552e2a
Jakub Grajber <grajber.jakub@gmail.com>
2023-07-03 07:23:57 +0200

    Init commi

---
+++ src/main/java/com/greybear/aws/demo/awspipelinedemo/AwsPipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.greybear.aws.demo.awspipelinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsPipelineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greybear/aws/demo/awspipelinedemo/AwsPipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greybear.aws.demo.awspipelinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
